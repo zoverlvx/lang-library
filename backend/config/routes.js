a5e50e91b24c929bbcf5d347a46e07807f805f04
zoverlvx <zoverhulser@gmail.com>
2019-06-11 23:09:23 +0000

    Lints routes file

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,12 +1,16 @@
-const {authenticate} = require("../auth/authenticate");
-const tokenService = require("../auth/tokenService");
+/*jslint
+    node
+*/
+
+// const {authenticate} = require("../auth/authenticate");
 const register = require("../auth/register.js");
 const login = require("../auth/login.js");
 
-module.exports = server => {
+module.exports = function (server) {
+    "use strict";
     server.post("/api/register", register);
     server.post("/api/login", login);
     // server.get("/api/languages", authenticate, getLanguages);
-}
+};
 
 
