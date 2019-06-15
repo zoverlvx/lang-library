39eaa22e464856db9c12064b7b924e258e8ad55f
zoverlvx <zoverhulser@gmail.com>
2019-06-15 21:58:19 +0000

    Lints jwt verifier.

--- backend/auth/authenticate.js
+++ backend/auth/authenticate.js
@@ -1,3 +1,34 @@
 /*jslint
  node
 */
+
+const jwt = require("jsonwebtoken");
+
+const jwtKey =
+        process.env.JWT_SECRET || "add a .env file to root of project with the JWT_SECRET variable";
+
+function authenticate(req, res, next) {
+    "use strict";
+    const token = req.get("Authorization");
+
+    if (token) {
+        jwt.verify(token, jwtKey, function (err, decoded) {
+            if (err) {
+                return res.status(401).json({error: err});
+            }
+
+            req.decoded = decoded;
+            next();
+        });
+    }
+
+    if (!token) {
+        return res.status(401).json({
+            error: "No token provided"
+        });
+    }
+}
+
+module.exports = {
+    authenticate
+};
