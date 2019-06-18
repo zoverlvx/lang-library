aead0b3d7a6d14a7888f5f0812d59e0f73559341
zoverlvx <zoverhulser@gmail.com>
2019-06-18 03:18:18 +0000

    Creates component for logging in.

---
+++ client/src/components/Login.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {connect} from "react-redux";
+import makeLogin from "./makers/makeForm";
+import {loginUser} from "../actions"
+
+const Login = makeLogin("handleLogin");
+
+function mapStateToProps(
+        {
+            registration: {
+                loggingIn,
+                error
+            }
+        }
+) {
+    return {
+        loggingIn,
+        error
+    }
+}
+
+export default connect(mapStateToProps, {loginUser})(Login);
