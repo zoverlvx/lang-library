4d2d1b4273e241f11d8f40958be53993c4babe9c
zoverlvx <zoverhulser@gmail.com>
2019-06-18 03:19:14 +0000

    Creates component for making forms.

---
+++ client/src/components/makers/makeForm.js
@@ -0,0 +1,30 @@
+import React from "react";
+import useAuth from "../hooks/useAuth";
+
+export default function(postHandler) {
+    return function(props) {
+        const [state, handleChange, o] = useAuth(props);
+        const handler = o[postHandler];
+
+        return (
+            <form onSubmit={handler}> 
+                <h1>{props.name}</h1>
+                <input 
+                    placeholder="username"
+                    name="username"
+                    type="text"
+                    value={state.username}
+                    onChange={handleChange}
+                />
+                <input 
+                    placeholder="password"
+                    name="password"
+                    type="password"
+                    value={state.password}
+                    onChange={handleChange}
+                />
+                <button type="submit">Submit<button>
+            </form>
+        );
+    }
+}
